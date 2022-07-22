53e6d60a4855bee23fe8680827713762fa02a966
MdRoki2020 <mroki815@gmail.com>
2022-07-23 00:56:05 +0600

    express fundamental

---
+++ express/index.js
@@ -0,0 +1,12 @@
+const express= require('express');
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send('Hello Express');
+})
+
+
+app.listen(5000,()=>{
+    console.log("Server Run Success");
+})
---
+++ express/routing.js
@@ -0,0 +1,20 @@
+const express= require('express');
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Home</h1>');
+})
+
+app.get('/about',(req,res)=>{
+    res.send('<h1>About</h1>');
+})
+
+app.get('/contact',(req,res)=>{
+    res.send('<h1>Contact</h1>');
+})
+
+
+app.listen(5000,()=>{
+    console.log("Server Run Success");
+})

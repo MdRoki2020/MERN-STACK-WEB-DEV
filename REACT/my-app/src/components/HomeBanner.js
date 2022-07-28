00c5cb1180fb37e1db786cde2d8e513c9fb598ae
MdRoki2020 <mroki815@gmail.com>
2022-07-28 11:00:07 +0600

    useEffect

--- REACT/my-app/src/components/HomeBanner.js
+++ REACT/my-app/src/components/HomeBanner.js
@@ -1,6 +1,20 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import Axios from 'axios';
 
 function HomeBanner(props) {
+
+
+  useEffect(()=>{
+    Axios.get('https://dummyjson.com/products').then((res)=>{
+
+      console.log(res.data);
+
+    }).catch((err)=>{
+
+      console.log(err);
+      
+    })
+  })
 
   // const [color,setColor]=useState('Red');
 
@@ -8,22 +22,28 @@ function HomeBanner(props) {
   //   setColor('Pink');
   // }
 
-  let UserName=useRef()
-  let Password=useRef()
+  // let UserName=useRef()
+  // let Password=useRef()
 
-  const click=()=>{
-    let u=UserName.current.value;
-    let p=Password.current.value;
+  // const click=()=>{
+  //   let u=UserName.current.value;
+  //   let p=Password.current.value;
 
-    alert(u)
-    alert(p)
-  }
+  //   alert(u)
+  //   alert(p)
+  // }
+
+
+
+
+
+
   return (
     <div>
       {/* <h2>{color}</h2> */}
-      <input ref={UserName} placeholder="UserName"/>
+      {/* <input ref={UserName} placeholder="UserName"/>
       <input ref={Password} placeholder="Password"/>
-      <button onClick={click}>Click</button>
+      <button onClick={click}>Click</button> */}
       {/* <button onClick={click}>Click</button> */}
     </div>
   )

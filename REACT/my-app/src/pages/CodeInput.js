98cdf778e5d12f5041e584f7b688379754b5b92d
MdRoki2020 <mroki815@gmail.com>
2022-07-30 14:38:34 +0600

    Data insert success

---
+++ REACT/my-app/src/pages/CodeInput.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactCodeInput from 'react-code-input'
+
+function CodeInput() {
+  return (
+    <div>
+      <ReactCodeInput type='number' fields={6} />
+    </div>
+  )
+}
+
+export default CodeInput
--- REACT/my-app/src/pages/Home.js
+++ REACT/my-app/src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import HomeBanner from '../components/HomeBanner'
+import CodeInput from './CodeInput'
+
 
 function Home() {
   return (
     <div>
       <HomeBanner title={{name:"roki",desc:"rock star"}}/>
+      <CodeInput />
     </div>
   )
 }

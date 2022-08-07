895fee17d3070eaf4801c86e10aa1437613f107a
MdRoki2020 <mroki815@gmail.com>
2022-08-08 01:14:25 +0600

    Set Defalut Image Name On Navbar

--- MERN TASK MANEGER PROJECT/mern-task-manager-2022-frontend/src/components/master-layout/MasterLayout.jsx
+++ MERN TASK MANEGER PROJECT/mern-task-manager-2022-frontend/src/components/master-layout/MasterLayout.jsx
@@ -5,7 +5,7 @@ import {AiFillProfile, AiOutlineMenuUnfold, AiOutlineLogout, AiFillDashboard} fr
 import { FaCreativeCommonsNd,FaUserPlus,FaTachometerAlt,FaRegTimesCircle,FaBraille } from "react-icons/fa";
 import logo from "../../assets/images/logo.svg";
 import roki from "../../assets/images/ROKI.jpg";
-import { removeSessions } from "../../helper/SessionHelper";
+import { getUserDetails, removeSessions } from "../../helper/SessionHelper";
 
 
 const MasterLayout = (props) => {
@@ -44,11 +44,11 @@ const MasterLayout = (props) => {
 
                     <div className="float-right h-auto d-flex">
                         <div className="user-dropdown">
-                            <img className="icon-nav-img icon-nav" src={roki} alt=""/>
+                            <img className="icon-nav-img icon-nav" src={getUserDetails()['photo']} alt=""/>
                             <div className="user-dropdown-content ">
                                 <div className="mt-4 text-center">
-                                    <img className="icon-nav-img" src={roki} alt=""/>
-                                    <h6>Roki</h6>
+                                    <img className="icon-nav-img" src={getUserDetails()['photo']} alt=""/>
+                                    <h6>{getUserDetails()['firstName']}</h6>
                                     <hr className="user-dropdown-divider  p-0"/>
                                 </div>
                                 <NavLink to="/Profile" className="side-bar-item">
